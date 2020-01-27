a19ef61c48c6f6915d0cc6a4ebfe7f058a38e4e8
Rodrigo <rodrigoramosrs2@hotmail.com>
2020-01-27 15:05:00 -0300

    Ajuste na dll do loader

--- ExternalModules/Loader.IISModule/Helper/AnalyticsManager.cs
+++ ExternalModules/Loader.IISModule/Helper/AnalyticsManager.cs
@@ -39,28 +39,29 @@ namespace Loader.Helper
 
         private static void DoPost(string Category , string ActionName , string Description, int AnalyticsType = 0)
         {
-            WebRequest httpWebRequest = WebRequest.Create(ConfigurationManager.LoaderURL + "api/Analytics/Send" );
 
-            // Create POST data and convert it to a byte array.  
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Method = "POST";
-
-            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-            {
-                var JsonObject = new { Category = Category, ActionName = ActionName, Description = Description, AnalyticsType = AnalyticsType };
-                string JSONString = JSONSerializer.ToJavaScriptObjectNotation(JsonObject);
-               /* string json = "{" +
-                                "\"Category\":\"" + Category + "\"," +
-                                "\"ActionName\":\"" + ActionName + "\"," +
-                                "\"Description\":\"" + Description + "\"," +
-                                "\"AnalyticsType\":" + AnalyticsType + 
-                                
-                              "}";*/
-
-                streamWriter.Write(JSONString);
-            }
             try
             {
+                WebRequest httpWebRequest = WebRequest.Create(ConfigurationManager.LoaderURL + "api/Analytics/Send");
+
+                // Create POST data and convert it to a byte array.  
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    var JsonObject = new { Category = Category, ActionName = ActionName, Description = Description, AnalyticsType = AnalyticsType };
+                    string JSONString = JSONSerializer.ToJavaScriptObjectNotation(JsonObject);
+                    /* string json = "{" +
+                                     "\"Category\":\"" + Category + "\"," +
+                                     "\"ActionName\":\"" + ActionName + "\"," +
+                                     "\"Description\":\"" + Description + "\"," +
+                                     "\"AnalyticsType\":" + AnalyticsType + 
+
+                                   "}";*/
+
+                    streamWriter.Write(JSONString);
+                }
                 var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
                 using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
                 {
